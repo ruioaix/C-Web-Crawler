7f3b1fca49059b2245a16295399b7aa1ea5d7c5d
Rui X <xrfind@gmail.com>
2015-09-20 15:27:31 +0200

    log and safe functions

---
+++ src/log.h
@@ -0,0 +1,39 @@
+#ifndef __LOG_H
+#define __LOG_H
+
+enum LOGLEVEL {
+	LOG_FATAL,
+	LOG_WARN,
+	LOG_OP,
+	LOG_INFO,
+	LOG_DBG,
+};
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LOG(level, ...) do {\
+	if (level <= loglevel) {\
+		if (logfile == stdout || logfile == stderr) { \
+			if (level==LOG_FATAL) fprintf(logfile, "[\033[7;31m%17s\033[1;37m:\033[1;34m%-4d \033[1;35m%12s()\033[0m] =>> ", __FILE__, __LINE__, __FUNCTION__);\
+			if (level==LOG_WARN)  fprintf(logfile, "[\033[1;33m%17s\033[1;37m:\033[1;34m%-4d \033[1;35m%12s()\033[0m] =>> ", __FILE__, __LINE__, __FUNCTION__);\
+			if (level==LOG_OP)    fprintf(logfile, "[\033[7;36m%17s\033[1;37m:\033[1;34m%-4d \033[1;35m%12s()\033[0m] =>> ", __FILE__, __LINE__, __FUNCTION__);\
+			if (level==LOG_INFO)  fprintf(logfile, "[\033[1;32m%17s\033[1;37m:\033[1;34m%-4d \033[1;35m%12s()\033[0m] =>> ", __FILE__, __LINE__, __FUNCTION__);\
+			if (level==LOG_DBG)   fprintf(logfile, "[\033[7;37m%17s\033[1;37m:\033[1;34m%-4d \033[1;35m%12s()\033[0m] =>> ", __FILE__, __LINE__, __FUNCTION__);\
+		} \
+		else { \
+			fprintf(logfile, "[%17s:%-4d %12s()] =>> ", __FILE__, __LINE__, __FUNCTION__);\
+		} \
+		fprintf(logfile, __VA_ARGS__);\
+		fprintf(logfile, "\n");\
+		fflush(logfile);\
+	}\
+	if (level == LOG_FATAL) {\
+		exit(-1);\
+	}\
+} while(0)
+
+enum LOGLEVEL loglevel = LOG_INFO;
+FILE *logfile;
+
+#endif
---
+++ src/safe.c
@@ -0,0 +1,42 @@
+#include "safe.h"
+#include "log.h"
+#include <stdlib.h>
+
+//memory 
+inline void *malloc_safe(size_t size, const char *filename, const int lineNum) {
+	if (size == 0) return NULL;
+	void *tmp = malloc(size);
+	if (tmp == NULL) {
+		LOG(LOG_FATAL, "malloc failed: \"%s\" file, %d line.\n",  filename, lineNum);
+	}
+	return tmp;
+}
+inline void *calloc_safe(size_t num, size_t size, const char *filename, const int lineNum) {
+	if (num == 0 || size == 0) return NULL;
+	void *tmp = calloc(num, size);
+	if (tmp == NULL) {
+		LOG(LOG_FATAL, "calloc failed: \"%s\" file, %d line.\n",  filename, lineNum);
+	}
+	return tmp;
+}
+inline void *realloc_safe(void *p, size_t size, const char *filename, const int lineNum) {
+	if (size == 0) {
+		LOG(LOG_WARN, "realloc size == 0, return the origin pointer");
+		return p;
+	}
+	void *tmp = realloc(p, size);
+	if (tmp == NULL) {
+		LOG(LOG_FATAL, "realloc failed: \"%s\" file, %d line.\n",  filename, lineNum);
+	}
+	return tmp;
+}
+
+//file
+FILE *fopen_safe(const char *openfilename, const char *mode, char *filename, const int lineNum) {
+	if (!openfilename) return NULL;
+	FILE *fp = fopen(openfilename, mode);
+	if (fp == NULL) {
+		LOG(LOG_FATAL, "fopen failed: \"%s\" file, %d line, can not open \"%s\"",  filename, lineNum, openfilename);
+	}
+	return fp;
+}
